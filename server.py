c39fdcecef64249a1f4edc5166d5f3bbadb450ac
great-jay-yoo <greatjs111@gmail.com>
2026-05-23 18:26:10 +0900

    원본

---
+++ server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import http.server
+import socketserver
+import os
+
+PORT = 8080
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/' or self.path == '':
+            self.path = '/index.html'
+        return super().do_GET()
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+with socketserver.TCPServer(("", PORT), MyHTTPRequestHandler) as httpd:
+    print(f"Server running at http://localhost:{PORT}/")
+    httpd.serve_forever()
